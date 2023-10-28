c3037febd179603ef65b98b4ce6b21e36298022e
GianmarcoDandrea <gianm96@hotmail.it>
2023-10-28 17:27:17 +0200

    update: deleted and rewrited most of the code, add new function, and formated the document

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const userChoiceElem = document.getElementById("selector");
 const playBtn = document.getElementById("play-button");
 
 // Quando premo il tasto gioca
-playBtn.addEventListener("click", function() {
+playBtn.addEventListener("click", function () {
 
     document.getElementById("grid").innerHTML = ``;
 
@@ -14,12 +14,12 @@ playBtn.addEventListener("click", function() {
     let widthSquare = "";
     let squareNumber = "";
 
-    if(userChoice === "easy") {
-        
+    if (userChoice === "easy") {
+
         squareNumber = 49;
         widthSquare = "dif-easy";
 
-    } else if(userChoice === "medium") {
+    } else if (userChoice === "medium") {
 
         squareNumber = 81;
         widthSquare = "dif-medium";
@@ -31,25 +31,64 @@ playBtn.addEventListener("click", function() {
 
     }
 
-    for (let i = 1; i <= squareNumber; i++) {
-        
-        document.getElementById("grid").innerHTML += `
-                                                    <div class="square ${widthSquare}">${i}</div>`
+    const numbersArray = genOrderedNumber(squareNumber);
+    console.log(numbersArray);
+
+    const gridElem = document.getElementById("grid");
+
+    for (let i = 0; i < numbersArray.length; i++) {
+
+        const curNumber = numbersArray[i];
+        //Genera lo square con il numero all'interno a la classe in base alla difficoltà 
+        const square = generateGridSquare(curNumber, widthSquare);
+        square.addEventListener("click", squareClick)
+
+        gridElem.append(square);
     }
 
 });
 
 
+
 /*******************************************/
 // FUNZIONI
+/**
+ * Restituisce array di maxNumber numeri tra 1 e maxNumber
+ * @param {numer} maxNumber
+ * @returns {array}
+ */
 function genOrderedNumber(maxNumber) {
-    let numberarray = "";
+    let arrayOrderedNum = [];
+
     for (let i = 1; i <= maxNumber; i++) {
-        console.log(i);
-        numberarray += parseInt(`${i}`);
+        arrayOrderedNum.push(i);
     }
-    return numberarray;
+    return arrayOrderedNum;
 }
+
+/**
+ * Dà al numero dell'array la classe square e la classe della larghezza in base alla difficoltà scelta, così da creare la cella con i bordi
+ * @param {numer} maxNumber
+ * @param {string} squareWidth
+ * @returns {array}
+ */
+function generateGridSquare(innerNumber, squareWidth) {
+    const newSquare = document.createElement("div");
+    newSquare.classList.add("square");
+    newSquare.classList.add(squareWidth);
+    newSquare.innerHTML = innerNumber;
+
+    return newSquare;
+}
+
+
+/**
+ * Funcione che dà la classe per il colore del background quando clicchiamo sullo square
+ */
+function squareClick() {
+    this.classList.add("clicked-bg");
+}
+
 
 
 
